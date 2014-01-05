d1d3ae2e3d345528f20ce1c08186e747a48206cd
sgpl <sharad45@gmail.com>
2014-01-05 23:44:10 +0300

    added a stupid simple program

--- chapter4/roughc4.rb
+++ chapter4/roughc4.rb
@@ -1,13 +1,27 @@
-# recursion ?
+# # recursion ?
 
-def _recursion num
-	if (num == 0) or (num < 0)
-		puts "number is zero"
+# def _recursion num
+# 	if (num == 0) or (num < 0)
+# 		puts "number is zero"
+# 	else
+# 		puts num
+# 		num -= 4
+# 		_recursion num
+# 	end
+# end
+
+# _recursion 14
+
+
+# get input from user
+input = gets.chomp.to_i
+while input >= 0
+	puts "input is currently: #{input}"
+	if input > 10 and input <= 20
+		input = input - 6
+	elsif input > 20 and input <= 30
+		input -= 12
 	else
-		puts num
-		num -= 4
-		_recursion num
+		input -= 1
 	end
 end
-
-_recursion 14
